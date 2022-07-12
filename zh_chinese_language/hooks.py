f02044c7df7c7a3dbff3c77ee1930b29040ac6cc
13302164071 <huzj@phipsoft.com>
2022-07-12 16:55:55 +0800

    Version-14

--- zh_chinese_language/hooks.py
+++ zh_chinese_language/hooks.py
@@ -16,7 +16,7 @@ app_license = "MIT"
 # app_include_css = "/assets/zh_chinese_language/css/zh_chinese_language.css"
 # app_include_js = "/assets/zh_chinese_language/js/zh_chinese_language.js"
 app_include_js = [
-    "/assets/js/zh.min.js"
+	"mfg.bundle.js"
 ]
 # include js, css files in header of web template
 # web_include_css = "/assets/zh_chinese_language/css/zh_chinese_language.css"
---
+++ zh_chinese_language/public/js/mfg.bundle.js
@@ -0,0 +1,13 @@
+import './translation';
+
+/*
+import './stock/doctype/item/item';
+import './bank_reconciliation_tool/number_card';
+import './frappe/view/calendar/calendar';
+import './frappe/format';
+import './frappe/ui/page';
+import './frappe/ui/filters/field_select';
+import './frappe/ui/toolbar/search_utils';
+import './frappe/ui/toolbar/about';
+import './frappe/roles_editor';
+*/
